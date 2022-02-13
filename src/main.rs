81459e5adbc9ecfc189f629fbab9a3756267f4d4
ONOUE Origami <onoueorigami@gmail.com>
2022-02-13 23:54:35 +0900

    はい

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,51 @@
+use actix_web::{web, App, HttpResponse, HttpServer};
+use serde::Deserialize;
+
 fn main() {
-    println!("{}", gcd(70, 40));
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(get_index))
+            .route("/gcd", web::post().to(post_gcd))
+    });
+    println!("Serving on http://localhost:3000...");
+    server.bind("127.0.0.1:3000")
+        .expect("Can not bind to port 3000")
+        .run()
+        .expect("Can not start server");
+}
+
+fn get_index() -> HttpResponse {
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(
+            r#"
+                <tltle>Rust Web Server</title>
+                    <form action="/gcd" method="post">
+                    <input type="text" name="n" />
+                    <input type="text" name="m" />
+                    <button type="submit">Compute GCD</button>
+                </form>
+            "#
+        )
+}
+
+#[derive(Deserialize)]
+struct GcdParameters {
+    n: u64,
+    m: u64,
+}
+
+fn post_gcd(form: web::Form<GcdParameters>) -> HttpResponse {
+    if form.n == 0 || form.n == 0 {
+        return HttpResponse::BadRequest()
+            .content_type("text/html")
+            .body("Please input valid numbers");
+    }
+
+    let response = format!("The greatest common divisor of {} and {} is {}", form.n, form.m, gcd(form.n, form.m));
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(response)
 }
 
 fn gcd(mut n: u64, mut m: u64) -> u64 {
