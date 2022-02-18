385fe2fc5ba18a33e25540f3bc71e3a66cec3c02
ONOUE Origami <onoueorigami@gmail.com>
2022-02-19 05:07:00 +0900

    はい

--- src/main.rs
+++ src/main.rs
@@ -1,188 +1,87 @@
-fn square_loop(mut x: f64) {
-    loop {
-        x = x * x
-    }
+#[derive(Debug)]
+struct Arguments {
+    target: String,
+    replacement: String,
+    filename: String,
+    output: String,
 }
 
-fn square_add_loop(c: f64) {
-    let mut x = 0.;
-    loop {
-        x = x * x + c
-    }
-}
+use text_colorizer::*;
 
-use num::Complex;
-
-fn complex_square_add_loop(c: Complex<f64>) {
-    let mut z = Complex { re: 0., im: 0. };
-    loop {
-        z = z * z + c
-    }
-}
-
-fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
-    let mut z = Complex { re: 0., im: 0. };
-    for i in 0..limit {
-        if z.norm_sqr() > 4. {
-            return Some(i);
-        }
-        z = z * z + c;
-    }
-    None
-}
-
-use std::str::FromStr;
-
-fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
-    match s.find(separator) {
-        None => None,
-        Some(index) => match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
-            (Ok(l), Ok(r)) => Some((l, r)),
-            _ => None,
-        },
-    }
-}
-
-#[test]
-fn test_parse_pair() {
-    assert_eq!(parse_pair::<i32>("", ','), None);
-    assert_eq!(parse_pair::<i32>("10,", ','), None);
-    assert_eq!(parse_pair::<i32>(",10", ','), None);
-    assert_eq!(parse_pair::<i32>("10, 20", ','), Some((10, 20)));
-    assert_eq!(parse_pair::<i32>("10, 20xy", ','), None);
-    assert_eq!(parse_pair::<f64>("0.5x", 'x'), None);
-    assert_eq!(parse_pair::<f64>("0.5x1.5", 'x'), Some((0.5, 1.5)));
-}
-
-fn parse_complex(s: &str) -> Option<Complex<f64>> {
-    match parse_pair(s, ',') {
-        Some((re, im)) => Some(Complex { re, im }),
-        None => None,
-    }
-}
-
-#[test]
-fn test_parse_complex() {
-    assert_eq!(
-        parse_complex("1.25,0.0625"),
-        Some(Complex {
-            re: 1.15,
-            im: 0.0625
-        })
+fn print_usage() {
+    eprintln!(
+        "{} - change occurrences of one string into another",
+        "quickreplace".green()
     );
-    assert_eq!(parse_complex(",-0.0625"), None);
-}
-
-fn pixel_to_point(
-    bounds: (usize, usize),
-    pixel: (usize, usize),
-    upper_left: Complex<f64>,
-    lower_right: Complex<f64>,
-) -> Complex<f64> {
-    let (width, height) = (
-        lower_right.re - upper_left.re,
-        upper_left.im - lower_right.im,
-    );
-
-    Complex {
-        re: upper_left.re + pixel.0 as f64 * width / bounds.0 as f64,
-        im: upper_left.im - pixel.1 as f64 * height / bounds.1 as f64,
-    }
-}
-
-#[test]
-fn test_pixel_to_point() {
-    assert_eq!(
-        pixel_to_point(
-            (100, 200),
-            (25, 175),
-            Complex { re: -1.0, im: 1.0 },
-            Complex { re: 1.0, im: -1.0 }
-        ),
-        Complex {
-            re: -0.5,
-            im: -0.75
-        }
-    )
-}
-
-fn render(
-    pixels: &mut [u8],
-    bounds: (usize, usize),
-    upper_left: Complex<f64>,
-    lower_right: Complex<f64>,
-) {
-    assert!(pixels.len() == bounds.0 * bounds.1);
-
-    for row in 0..bounds.1 {
-        for column in 0..bounds.0 {
-            let point = pixel_to_point(bounds, (column, row), upper_left, lower_right);
-            pixels[row * bounds.0 + column] = match escape_time(point, 255) {
-                None => 0,
-                Some(count) => 255 - count as u8,
-            };
-        }
-    }
-}
-
-use image::png::PNGEncoder;
-use image::ColorType;
-use std::fs::File;
-
-fn write_image(
-    filename: &str,
-    pixels: &[u8],
-    bounds: (usize, usize),
-) -> Result<(), std::io::Error> {
-    let output = File::create(filename)?;
-
-    let encoder = PNGEncoder::new(output);
-    encoder.encode(pixels, bounds.0 as u32, bounds.1 as u32, ColorType::Gray(8))?;
-    Ok(())
+    eprintln!("Usage: quickreplace <target> <replacement> <INPUT> <OUTPUT>");
 }
 
 use std::env;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
+fn parse_args() -> Arguments {
+    let args: Vec<String> = env::args().skip(1).collect();
 
-    if args.len() != 5 {
-        eprintln!("usage: {} FILE PIXELS UPPERLEFT LOWERRIGHT", args[0]);
+    if args.len() != 4 {
+        print_usage();
         eprintln!(
-            "Example: {}, mandel.png 1000x750 -1.20,0.35 -1,0.20",
-            args[0]
+            "{} wrong number of arguments: expected 4, but got {}.",
+            "Error:".red().bold(),
+            args.len()
         );
         std::process::exit(1);
     }
 
-    let bounds = parse_pair(&args[2], 'x').expect("Error parsing image dimensions");
-    let upper_left = parse_complex(&args[3]).expect("Error parsing upper left corner point");
-    let lower_right = parse_complex(&args[4]).expect("Error parsing lower right corner point");
+    Arguments {
+        target: args[0].clone(),
+        replacement: args[1].clone(),
+        filename: args[2].clone(),
+        output: args[3].clone(),
+    }
+}
 
-    let mut pixels = vec![0; bounds.0 * bounds.1];
+use regex::Regex;
 
-    //render(&mut pixels, bounds, upper_left, lower_right);
-    let threads = 8;
-    let rows_per_band = bounds.1 / threads + 1;
+fn replace(target: &str, replacement: &str, text: &str) -> Result<String, regex::Error> {
+    let regex = Regex::new(target)?;
+    Ok(regex.replace_all(text, replacement).to_string())
+}
 
-    {
-        let bands: Vec<&mut [u8]> = pixels.chunks_mut(rows_per_band * bounds.0).collect();
-        crossbeam::scope(|spawner| {
-            for (i, band) in bands.into_iter().enumerate() {
-                let top = rows_per_band * i;
-                let height = band.len() / bounds.0;
-                let band_bounds = (bounds.0, height);
-                let band_upper_left = pixel_to_point(bounds, (0, top), upper_left, lower_right);
-                let band_lower_right =
-                    pixel_to_point(bounds, (bounds.0, top + height), upper_left, lower_right);
+use std::fs;
 
-                spawner.spawn(move |_| {
-                    render(band, band_bounds, band_upper_left, band_lower_right);
-                });
-            }
-        })
-        .unwrap()
+fn main() {
+    let args = parse_args();
+
+    let data = match fs::read_to_string(&args.filename) {
+        Ok(v) => v,
+        Err(e) => {
+            eprintln!(
+                "{} failed to read from file '{}' : {:?}",
+                "Error:".red().bold(),
+                args.filename,
+                e
+            );
+            std::process::exit(1);
+        }
+    };
+
+    let replaced_data = match replace(&args.target, &args.replacement, &data) {
+        Ok(v) => v,
+        Err(e) => {
+            eprintln!("{} failed to replace text: {:?}", "Error:".red().bold(), e);
+            std::process::exit(1);
+        }
     }
 
-    write_image(&args[1], &pixels, bounds).expect("Error writing PNG file");
+    match fs::write(&args.output, &data) {
+        Ok(_) => {}
+        Err(e) => {
+            eprintln!(
+                "{} failed to write to file '{}': {:?}",
+                "Error:".red().bold(),
+                args.filename,
+                e
+            );
+            std::process::exit(1);
+        }
+    };
 }
